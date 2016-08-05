73e76495fedc98ccd2f718ad559a50ebd2967f22
Amiram Wingarten <amiramw@gmail.com>
2016-08-05 15:55:41 +0300

    Format code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,47 @@
 'use strict';
 module.exports = function (grunt) {
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'tasks/**/*.js',
-        '<%= nodeunit.tests %>'
-      ]
-    },
-    clean: {
-      test: [
-        'test/tmp'
-      ]
-    },
-    nodeunit: {
-      tests: ['test/test.js']
-    },
-    sass: {
-      compile: {
-        files: {
-          'test/tmp/fixtures/banner.css': 'test/fixtures/banner.scss',
-          'test/tmp/fixtures/compile.css': 'test/fixtures/compile.scss',
-          'test/tmp/fixtures/imported.css': 'test/fixtures/imported.scss'
-        }
-      }
-    }
-  });
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'tasks/**/*.js',
+				'<%= nodeunit.tests %>'
+			]
+		},
+		clean: {
+			test: [
+				'test/tmp'
+			]
+		},
+		nodeunit: {
+			tests: ['test/test.js']
+		},
+		sass: {
+			compile: {
+				files: {
+					'test/tmp/fixtures/banner.css': 'test/fixtures/banner.scss',
+					'test/tmp/fixtures/compile.css': 'test/fixtures/compile.scss',
+					'test/tmp/fixtures/imported.css': 'test/fixtures/imported.scss'
+				}
+			}
+		}
+	});
 
-  grunt.loadTasks('tasks');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-internal');
+	grunt.loadTasks('tasks');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-internal');
 
-  grunt.registerTask('test', [
-    'clean',
-    'jshint',
-    'sass',
-    'nodeunit',
-    'clean'
-  ]);
-  grunt.registerTask('default', ['test', 'contrib-core', 'contrib-ci:skipIfExists']);
+	grunt.registerTask('test', [
+		'clean',
+		'jshint',
+		'sass',
+		'nodeunit',
+		'clean'
+	]);
+	grunt.registerTask('default', ['test', 'contrib-core', 'contrib-ci:skipIfExists']);
 };
--- test/test.js
+++ test/test.js
@@ -3,18 +3,18 @@
 var grunt = require('grunt');
 
 function readFile(file) {
-  var contents = grunt.file.read(file);
-  return process.platform === 'win32' ?  contents.replace(/\r\n/g, '\n') : contents;
+	var contents = grunt.file.read(file);
+	return process.platform === 'win32' ? contents.replace(/\r\n/g, '\n') : contents;
 }
 
 exports.sass = {
-  compileScss: function (test) {
-    test.equal(readFile('test/tmp/fixtures/banner.css'), readFile('test/fixtures/banner.css'), 'banner should compile SCSS to CSS');
-    test.equal(readFile('test/tmp/fixtures/compile.css'), readFile('test/fixtures/compile.css'), 'compile should compile SCSS to CSS');
-    test.equal(readFile('test/tmp/fixtures/imported.css'), readFile('test/fixtures/imported.css'), 'imported should compile SCSS to CSS');
+	compileScss: function (test) {
+		test.equal(readFile('test/tmp/fixtures/banner.css'), readFile('test/fixtures/banner.css'), 'banner should compile SCSS to CSS');
+		test.equal(readFile('test/tmp/fixtures/compile.css'), readFile('test/fixtures/compile.css'), 'compile should compile SCSS to CSS');
+		test.equal(readFile('test/tmp/fixtures/imported.css'), readFile('test/fixtures/imported.css'), 'imported should compile SCSS to CSS');
 
-    test.ok(!grunt.file.exists('test/tmp/_partial.css'), 'underscore partial files should be ignored');
+		test.ok(!grunt.file.exists('test/tmp/_partial.css'), 'underscore partial files should be ignored');
 
-    test.done();
-  }
+		test.done();
+	}
 };
